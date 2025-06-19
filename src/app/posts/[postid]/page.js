33e55e992c6d488dfabc15eec8c0fe6c0386e14b
Ross Fletcher <rossfletcher108@gmail.com>
2025-06-19 11:32:04 +0100

    setup

---
+++ src/app/posts/[postid]/page.js
@@ -0,0 +1,12 @@
+export default function PostIdPage() {
+  // TODO: Get the entry data from the database for each individual post to render them dynmanically
+
+  return (
+    <>
+      <h1>This is the individual post page</h1>
+      {/* TODO: Render the individuals post information */}
+      {/* TODO: Set up a comments form --> this could be seperate component */}
+      {/* TODO: render a list of comments */}
+    </>
+  );
+}
---
+++ src/app/posts/page.js
@@ -0,0 +1,7 @@
+export default function postPage() {
+  return (
+    <>
+      <h1>This is the post page</h1>
+    </>
+  );
+}
