5ec2c2c153751e016e502a4ecd2735eb33992c9c
Ross Fletcher <rossfletcher108@gmail.com>
2025-06-20 11:36:04 +0100

    added link from database to local images

---
+++ src/lib/data.js
@@ -0,0 +1,14 @@
+// This are my local images, with an id that matches the post id in my database
+
+const data = [
+  {
+    id: 3,
+    src: "/images/ahscollagen.jpg",
+  },
+  {
+    id: 4,
+    src: "/images/ahscollagen.jpg",
+  },
+];
+
+export default data;
