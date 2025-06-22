38b407c371d5cd07afa9836341ef540f28edcfb3
Ross Fletcher <rossfletcher108@gmail.com>
2025-06-22 16:54:45 +0100

    added mobile css

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <div className={footerStyles.footer}>
-      <Link href={`/casestudies`} className={footerStyles.link}>
+      <Link href={`https://www.linkedin.com/in/ross-fletcher-12a31b1b3/`} className={footerStyles.link}>
         <h1>LinkedIn</h1>
       </Link>
-      <Link href={`/casestudies`} className={footerStyles.link}>
+      <Link href={`https://github.com/rosdykat`} className={footerStyles.link}>
         <h1>Github</h1>
       </Link>
     </div>
